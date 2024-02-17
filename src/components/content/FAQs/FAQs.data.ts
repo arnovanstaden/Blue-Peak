7aeb56afa4913c0720a94c3624f1fcc3ebf9a01b
arnovanstaden <arnovanstaden@gmail.com>
2024-02-17 09:23:39 +0100

    faq section

---
+++ src/components/content/FAQs/FAQs.data.ts
@@ -0,0 +1,10 @@
+const faqData = [{
+  question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+  answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada.'
+},
+{
+  question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+  answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada.'
+}];
+
+export default faqData;
