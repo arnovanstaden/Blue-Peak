8ed59f3e24903016ba924f53e23f92fdcf2528de
arnovanstaden <arnovanstaden@gmail.com>
2024-02-17 10:53:10 +0100

    Add service images

--- src/components/content/ServicesOverview/ServicesOverview.data.ts
+++ src/components/content/ServicesOverview/ServicesOverview.data.ts
@@ -1,19 +1,23 @@
 export const servicesOverviewData = [
   {
     name: 'Plumbing',
-    description: 'lorem ipsum dolor sit amet'
+    description: 'lorem ipsum dolor sit amet',
+    image: '/static/images/plumbing.jpg'
   },
   {
     name: 'Heating',
-    description: 'lorem ipsum dolor sit amet'
+    description: 'lorem ipsum dolor sit amet',
+    image: '/static/images/heating.jpeg'
   },
   {
     name: 'Gas',
-    description: 'lorem ipsum dolor sit amet'
+    description: 'lorem ipsum dolor sit amet',
+    image: '/static/images/gas.jpeg'
   },
   {
     name: 'Certificates of Compliance',
-    description: 'lorem ipsum dolor sit amet'
+    description: 'lorem ipsum dolor sit amet',
+    image: '/static/images/coc.jpeg'
   },
 ]
 
