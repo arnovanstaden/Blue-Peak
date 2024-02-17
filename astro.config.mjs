50f79edea51f04bd45aaf4830f1aa4a21bce0001
arnovanstaden <arnovanstaden@gmail.com>
2024-02-17 13:12:02 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000
   },
+  prefetch: true,
   vite: {
     css: {
       preprocessorOptions: {
@@ -17,5 +19,6 @@ export default defineConfig({
       }
     }
   },
-  integrations: [react()],
+  site: 'https://www.blue-peak.co.za',
+  integrations: [react(), sitemap()]
 });
